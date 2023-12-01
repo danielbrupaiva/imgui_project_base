f20d7ee1c22420dfe9f4616e48a5030df0e85e21
Daniel Paiva <danielbrug@gmail.com>
2023-12-01 20:49:21 -0300

    Refactor FileLogger class as singleton

--- Core/filelogger.hpp
+++ Core/filelogger.hpp
@@ -5,58 +5,43 @@
 #include <memory>
 #include <ctime>
 #include <chrono>
-#include <string_view>
+#include <map>
 
 #define PRINT(x) std::cout << x << std::endl;
 
 namespace Core {
 
-class FileLogger
+class Logger
 {
 public:
-    enum class LogLevel{
+    enum class Level
+    {
         INFO,
         WARNING,
         ERROR
     };
-    std::string TAG = "FILELOGGER";
-    std::string m_FileName;
-    LogLevel m_LogLevel;
+
 public:
-    ~FileLogger(){
-        LOG(LogLevel::INFO, TAG, "Logger file closed");
-        FILEDeleter();
-    };
-    FileLogger(const std::string& fileName)
-        : m_FileName(fileName)
+    static Logger &Instance()
     {
-        LOG(LogLevel::INFO, TAG, "Load logger file");
-    };
-    void LOG(const std::string& message)
+        static Logger *s_instance = new Logger;
+        return *s_instance;
+    }
+
+    inline void LOG(const std::string message)
     {
-        LOG(LogLevel::INFO, "", message);
+        LOG(Level::INFO, "", message);
     };
 
-    void LOG(const LogLevel logLevel, const std::string& TAG, const std::string& message)
+    inline void LOG(const Level logLevel, const std::string &TAG, const std::string message)
     {
-        std::string levelStr;
-
-        switch (logLevel)
+        // Create a smart pointer with custom deleter
+        std::shared_ptr<FILE> file(std::fopen(this->m_FileName.c_str(), "a"), FILEDeleter());
+        if (!file)
         {
-        case LogLevel::ERROR:
-            levelStr = "[ERROR]";
-            break;
-        case LogLevel::INFO:
-            levelStr = "[INFO]";
-            break;
-        case LogLevel::WARNING:
-            levelStr = "[WARNING]";
-            break;
-        default:
-            levelStr = "";
-            break;
+            std::cerr << "Failed to open the file." << std::endl;
+            return;
         }
-
         // Get the current system time
         auto currentTime = std::chrono::system_clock::now();
         // Convert the time to a time_t object
@@ -64,31 +49,50 @@ public:
         // Convert the time_t to a string representation
         char timeString[100];
         std::strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S.%s", std::localtime(&time)); // ISO 8601 https://en.cppreference.com/w/c/chrono/strftime
-
+        // String builder
         std::string _message;
-        if(TAG!=""){
-            _message = "[" + static_cast<std::string>(timeString) + "]" + levelStr + "[" + TAG + "]" + message+"\n";
-        }else{
-            _message = "[" + static_cast<std::string>(timeString) + "]" + levelStr +  message+"\n";
+        if (TAG != "")
+        {
+            _message = "[" + static_cast<std::string>(timeString) + "][" + levelStr[logLevel] + "][" + TAG + "]" + message + "\n";
         }
-        PRINT(_message);
-        //Create a smart pointer with custom deleter
-        std::shared_ptr<FILE> file(std::fopen(this->m_FileName.c_str(),"a"),FILEDeleter() );
-        if(!file){
-            std::cerr << "Failed to open the file." << std::endl;
-            return;
+        else
+        {
+            _message = "[" + static_cast<std::string>(timeString) + "][" + levelStr[logLevel] + "]" + message + "\n";
         }
-        std::fprintf( file.get(),"%s", _message.c_str() );
+        // Flush message to file
+        std::fprintf(file.get(), "%s", _message.c_str());
     };
 
-    struct FILEDeleter{
-        void operator()(FILE *pFile){
-            if(pFile){
+    inline void SetFileName(const std::string &_filename) { m_FileName = _filename; };
+    inline std::string GetFileName() const { return m_FileName; };
+
+private:
+    Logger(){};
+    ~Logger(){};
+    // Custom deleter
+    struct FILEDeleter
+    {
+        void operator()(FILE *pFile)
+        {
+            if (pFile)
+            {
                 fclose(pFile);
-                //print("File close.");
             }
         }
     };
+    // Prevent copying
+    Logger(const Logger &) = delete;
+    Logger &operator=(const Logger &) = delete;
+
+    std::string m_TAG = "Logger";
+    std::string m_FileName;
+    Level m_LogLevel;
+
+    std::map<Level, std::string> levelStr = {
+        {Level::INFO, "INFO"},
+        {Level::WARNING, "WARNING"},
+        {Level::ERROR, "ERROR"},
+        };
 };
 
-}
+};//namespace Core
